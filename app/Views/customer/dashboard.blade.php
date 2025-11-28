7ec883de53cffb18703b76118bd1215225384bfc
Rifatx990 <158224402+Rifatx990@users.noreply.github.com>
2025-11-28 21:30:02 +0600

    Create dashboard.blade.php

---
+++ app/Views/customer/dashboard.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h1>Customer Dashboard</h1>
+                <p>Welcome, {{ auth()->user()->name }}!</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-3">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Products</h5>
+                        <p class="card-text">{{ $products->count() }}</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Orders</h5>
+                        <p class="card-text">{{ $orders->count() }}</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Custom Orders</h5>
+                        <p class="card-text">{{ $customOrders->count() }}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
