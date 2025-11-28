7c9fa85a1e548cd89bc316412fad737b191e88e5
Rifatx990 <158224402+Rifatx990@users.noreply.github.com>
2025-11-28 21:26:21 +0600

    Add authentication configuration in Auth.php

---
+++ app/Helpers/config/Auth.php
@@ -0,0 +1,19 @@
+'guards' => [
+    'web' => [
+        'driver' => 'session',
+        'provider' => 'users',
+    ],
+
+    'api' => [
+        'driver' => 'token',
+        'provider' => 'users',
+        'hash' => false,
+    ],
+],
+
+'providers' => [
+    'users' => [
+        'driver' => 'eloquent',
+        'model' => App\Models\User::class,
+    ],
+],
