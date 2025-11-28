99e05f41cbd6dcd4b27a8efdcdabdc292a0b4b95
Rifatx990 <158224402+Rifatx990@users.noreply.github.com>
2025-11-28 21:05:22 +0600

    Create OrderController.php

---
+++ app/Controllers/OrderController.php
@@ -0,0 +1,50 @@
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Order;
+
+class OrderController extends Controller
+{
+    public function index()
+    {
+        $orders = Order::all();
+
+        return view('orders.index', compact('orders'));
+    }
+
+    public function create()
+    {
+        return view('orders.create');
+    }
+
+    public function store(Request $request)
+    {
+        $order = Order::create($request->all());
+
+        return redirect()->route('orders.index');
+    }
+
+    public function show(Order $order)
+    {
+        return view('orders.show', compact('order'));
+    }
+
+    public function edit(Order $order)
+    {
+        return view('orders.edit', compact('order'));
+    }
+
+    public function update(Request $request, Order $order)
+    {
+        $order->update($request->all());
+
+        return redirect()->route('orders.index');
+    }
+
+    public function destroy(Order $order)
+    {
+        $order->delete();
+
+        return redirect()->route('orders.index');
+    }
+}
